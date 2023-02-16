fbb23b64e078fe73d3ccde298abf1be617569001
Симонов Алексей <alxxalxx@mail.ru>
2023-02-16 16:07:13 +0300

    Fix code with reviewr notes

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,15 +1,12 @@
 # ice_cream/urls.py
 from django.urls import path
+
 from . import views
 
-app_name = 'posts_1'
+app_name = 'posts'
 
 urlpatterns = [
 
     path('', views.index, name='posts_main_padge'),
-    path('group/<slug:pk>/', views.posts_list),
-
-    path('justpage/', views.JustStaticPage.as_view()),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('group/<slug:slug>/', views.posts_list),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group, User
-from django.core.paginator import Paginator
-from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+PAGES: int = 10
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-pub_date')
-    # Если порядок сортировки определен в классе Meta модели,
-    # запрос будет выглядеть так:
-    # post_list = Post.objects.all()
-    # Показывать по 10 записей на странице.
-    paginator = Paginator(post_list, 10)
-
-    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
-    page_number = request.GET.get('page')
-
-    # Получаем набор записей для страницы с запрошенным номером
-    page_obj = paginator.get_page(page_number)
-    # Отдаем в словаре контекста
+    posts = Post.objects.all()[:PAGES]
     context = {
-        'page_obj': page_obj,
+        'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
@@ -28,41 +17,9 @@ def posts_list(request, pk):
 
     group = get_object_or_404(Group, slug=pk)
 
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).all()[:PAGES]
     context = {
         'group': group,
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
-
-class JustStaticPage(TemplateView):
-    template_name = 'posts/just_page.html'
-
-
-def profile(request, username):
-    # Здесь код запроса к модели и создание словаря контекста
-    author = get_object_or_404(User, username=username)
-    posts = author.posts.all()
-    paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    posts_count = posts.count()
-    context = {
-        'page_obj': page_obj,
-        'author': author,
-        'posts_count': posts_count,
-    }
-    return render(request, 'posts/profile.html', context)
-
-
-def post_detail(request, post_id):
-    # Здесь код запроса к модели и создание словаря контекста
-    post = get_object_or_404(Post, pk=post_id)
-    posts = Post.objects.filter(author__username=post.author.get_username)
-    posts_count = posts.count()
-    context = {
-        'post': post,
-        'posts_count': posts_count,
-    }
-    return render(request, 'posts/post_detail.html', context)
